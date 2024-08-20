34100af0380156018761746424cc42d720ddb3f5
boukaii <guillaume271293@hotmail.fr>
2024-08-20 20:47:53 +0200

    modification css

--- src/components/competence/Competences.jsx
+++ src/components/competence/Competences.jsx
@@ -5,7 +5,7 @@ function Competences() {
     return (
         <div className='global-compet' id='nav-compet'>
             <h2 className="title-competence">Technologies et outils</h2>
-            <h3 className="p-compt">Voici les différents languages et outils que je maitrise</h3>
+            <h3 className="p-compt">Voici les différents languages et outils que je maîtrise</h3>
             <div className="CP1">
                 <div className="test">
                     <img src="https://www.vectorlogo.zone/logos/php/php-ar21.svg" alt="" />
@@ -31,31 +31,8 @@ function Competences() {
                 <div className="test">
                     <img src="https://www.vectorlogo.zone/logos/w3_css/w3_css-ar21.svg" alt="" />
                 </div>
-                
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-ar21.svg" alt="" />
-                </div>
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/docker/docker-ar21.svg" alt="" />
-                </div>
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg" alt="" />
-                </div>
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/heroku/heroku-ar21.svg" alt="" />
-                </div>
-         
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg" alt="" />
-                </div>
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/github/github-ar21.svg" alt="" />
-                </div>
                 <div className="test">
                 <img src="https://www.vectorlogo.zone/logos/mysql/mysql-horizontal.svg" alt="" />
-                </div>
-                <div className="test">
-                    <img src="https://www.vectorlogo.zone/logos/postgresql/postgresql-horizontal.svg" alt="" />
                 </div>
             </div>
         </div>
