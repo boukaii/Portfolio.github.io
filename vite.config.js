1030905d38b29abfca4d91c7e2fea96930443743
boukaii <guillaume271293@hotmail.fr>
2024-08-31 08:45:14 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // });
 
-export default {
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-simple-typewriter'],
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-simple-typewriter'],
+      // external: ['react-simple-typewriter'],  // Commenter ou enlever cette ligne
     },
   },
-};
+});
+
